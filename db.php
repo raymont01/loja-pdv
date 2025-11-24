07d17de37001479be0501d2d24fa196efcf19425
Rayssa <rayssa.vanartec@gmail.com>
2025-11-23 21:30:30 -0300

    Database

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$DB_HOST = '127.0.0.1';
+$DB_NAME = 'produtos_db';
+$DB_USER = 'root';
+$DB_PASS = '';
+
+$options = [PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC];
+try{
+  $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8mb4", $DB_USER, $DB_PASS, $options);
+}
+catch(PDOException $e){ 
+  http_response_code(500); echo "Falha na conexão com BD: " . $e->getMessage(); exit; 
+}
