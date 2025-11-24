83f377a46d15ee0635777c1c77e71a0823f616fd
Aquiles Francisco da Silva <aquilessilva668@gmail.com>
2025-11-24 14:02:17 -0300

    Cria navbar fixa com links para PDV, produtos e clientes

---
+++ navbar.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">PDV Simples</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Vendas (PDV)</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="cadastro_produtos.php">Cadastrar Produtos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="cadastro_clientes.php">Cadastrar Clientes</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
